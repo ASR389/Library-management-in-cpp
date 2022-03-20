90c47f74f3363b7ffaf4c697f054144c7282f3f4
Ajay <97617151+ASR389@users.noreply.github.com>
2022-03-21 00:26:22 +0530

    Commit

---
+++ class.h
@@ -0,0 +1,25 @@
+/* class.h */
+
+#define ADMINUSER "Teacher"
+#define ADMINPASSWORD "1234"
+
+#define USER "Student"
+#define USERPASSWORD "1234"
+
+#define FINE 500
+
+int addBook();
+
+int deleteBook();
+
+int updateBook();
+
+int listOfAllBooks();
+
+int borrowBook();
+
+int returnBook();
+
+int adminLogin();
+
+int userLogin();
